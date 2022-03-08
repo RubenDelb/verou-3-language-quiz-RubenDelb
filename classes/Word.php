f72f7bdf38c16940b0fde6eb0f077248f206169f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-08 15:42:32 +0100

    Answers with uppercases will also be counted correct

--- classes/Word.php
+++ classes/Word.php
@@ -15,12 +15,12 @@ class Word
     public function verify(string $answer): bool
     {
         // Use this function to verify if the provided answer by the user matches the correct one
-            if ($answer === $this->answer) {
+            // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
+            if (strtolower($answer) === $this->answer) {
                 return true;
             } else {
                 return false;
             }
-    }
-        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-        // Bonus (hard): can you allow answers with small typo's (max one character different)?
+    } 
+        // TODO: Bonus (hard): can you allow answers with small typo's (max one character different)?
 }
