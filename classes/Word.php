29596c7358e331c1e4bd2c3a958a7e02b8598dfa
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-08 11:38:21 +0100

    WIP add verify function to check the guessed answer

--- classes/Word.php
+++ classes/Word.php
@@ -8,14 +8,21 @@ class Word
 
     public function __construct(string $word, string $answer)
     {
-        $this->word = $word;
-        $this->answer = $answer;
+        $this->word = strtolower($word);
+        $this->answer = strtolower($answer);
     }
 
-    // public function verify(string $answer): bool
-    // {
-    //     // TODO: use this function to verify if the provided answer by the user matches the correct one
-    //     // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-    //     // Bonus (hard): can you allow answers with small typo's (max one character different)?
-    // }
+    public function verify(string $answer): bool
+    {
+        // TODO: use this function to verify if the provided answer by the user matches the correct one
+            if ($_POST["guessedAnswer"] === $this->answer) {
+                echo "CORRECT ";
+                return true;
+            } else {
+                echo "PEEEUUTTT";
+                return false;
+            }
+    }
+        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
+        // Bonus (hard): can you allow answers with small typo's (max one character different)?
 }
