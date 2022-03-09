9e45751c4e223c5bbf67dcd031180b1309bf4545
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-09 10:16:28 +0100

    Create session variables to store the player information

--- classes/Player.php
+++ classes/Player.php
@@ -5,11 +5,24 @@ class Player
     // Add name and score
     public string $name;
     public int $score;
+    public int $mistakes;
 
-    public function __construct(string $name, int $score)
+    public function __construct(string $name, int $score, int $mistakes)
     {
         // Add 👤 automatically to their name
         $this->name = '👤' . $name;
         $this->score = $score;
+        $this->mistakes = $mistakes;
     }
+}
+
+if (isset($_POST['name'])) {
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['score'] = 0;
+    $_SESSION['mistakes'] = 0;
+}
+
+if (isset($_SESSION['name'])) {
+    $newPlayer = new Player($_SESSION['name'], $_SESSION['score'], $_SESSION['mistakes']);
+    // pre($newPlayer);
 }
